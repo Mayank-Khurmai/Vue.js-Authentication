85ed21043c2559699cdb9c74e5afd5c1a5326dfd
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-12-26 20:47:30 +0530

    Update authenticate.vue

--- authenticate.vue
+++ authenticate.vue
@@ -1,4 +1,43 @@
-const user ={
+const authenticate = {
+	template: `
+	<img src="https://swoopnow.com/wp-content/uploads/2017/12/user-authentication.png" class="w-100">
+	<h4 class="my-4">{{ response }}</h4>
+	<button class="btn btn-primary rounded-0" v-on:click="auth">Try authentication</button>
+	`,
+	data(){
+		return{
+			response: "",
+			username: this.$route.params.user,
+			password: this.$route.params.password,
+		}
+	},
+
+	methods: {
+		auth(){
+			this.response = "Authenticating please wait...";
+			const formdata = new FormData();
+			formdata.append("username",this.username);
+			formdata.append("password",this.password);
 
 
+			const ajax = axios({
+				method: "POST",
+				url: "data.php",
+				data: formdata,
+			});
+
+			ajax.then(function(r){
+				var message = r.data;
+				if(message.trim() == "success")
+				{
+					this.response = "Hi "+this.username;
+				}
+
+				else{
+					this.response = "Unauthorized user !";
+				}
+			}.bind(this));
+		}
+	}
 }
+
